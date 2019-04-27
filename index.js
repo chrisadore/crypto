39cf0e630ba58695a3f1681f82d599df8b7530fd
chrisadore <chris.isadore@gmail.com>
2019-04-27 07:17:19 +0530

    done

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const express = require('express');
 const request = require('request');
 const Blockchain = require('./blockchain');
 const PubSub = require('./app/pubsub');
+const TransactionPool = require('./wallet/transaction-pool');
+const Wallet = require('./wallet');
+const TransactionMiner = require('./app/transaction-miner');
 
 const app = express();
 const blockchain = new Blockchain();
-const pubsub = new PubSub({ blockchain });
+const transactionPool = new TransactionPool();
+const wallet = new Wallet();
+const pubsub = new PubSub({ blockchain, transactionPool });
+const transactionMiner = new TransactionMiner({ blockchain, transactionPool, wallet, pubsub});
 
 const DEFAULT_PORT = 3000;
 const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
@@ -27,7 +33,71 @@ app.post('/api/mine', (req, res) => {
     res.redirect('/api/blocks');
 });
 
-const syncChains = () => {
+app.post('/api/transact', (req, res) => {
+    const { amount, recipient } = req.body;
+  
+    let transaction = transactionPool
+      .existingTransaction({ inputAddress: wallet.publicKey });
+  
+    try {
+      if (transaction) {
+        transaction.update({ senderWallet: wallet, recipient, amount });
+      } else {
+        transaction = wallet.createTransaction({
+          recipient,
+          amount,
+          chain: blockchain.chain
+        });
+      }
+    } catch(error) {
+      return res.status(400).json({ type: 'error', message: error.message });
+    }
+  
+    transactionPool.setTransaction(transaction);
+  
+    pubsub.broadcastTransaction(transaction);
+  
+    res.json({ type: 'success', transaction });
+  });
+
+  app.get('/api/transaction-pool-map', (req, res) => {
+    res.json(transactionPool.transactionMap);
+  });
+
+  app.get('/api/mine-transactions', (req, res) => {
+    transactionMiner.mineTransactions();
+  
+    res.redirect('/api/blocks');
+  });
+
+  app.get('/api/wallet-info', (req, res) => {
+    const address = wallet.publicKey;
+  
+    res.json({
+      address,
+      balance: Wallet.calculateBalance({ chain: blockchain.chain, address })
+    });
+  });
+  
+  app.get('/api/known-addresses', (req, res) => {
+    const addressMap = {};
+  
+    for (let block of blockchain.chain) {
+      for (let transaction of block.data) {
+        const recipient = Object.keys(transaction.outputMap);
+  
+        recipient.forEach(recipient => addressMap[recipient] = recipient);
+      }
+    }
+  
+    res.json(Object.keys(addressMap));
+  });
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/dist/index.html'));
+  });
+  
+const syncWithRootState = () => {
     request ({ url: `${ROOT_NODE_ADDRESS}/api/blocks`}, (error, response, body) => {
         if(!error && response.statusCode == 200) {
             const rootChain = JSON.parse(body);
@@ -35,6 +105,15 @@ const syncChains = () => {
             console.log('replace chain on a sync with', rootChain);
             blockchain.replaceChain(rootChain);
         }
+    });
+
+request({ url: `${ROOT_NODE_ADDRESS}/api/transaction-pool-map` }, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+        const rootTransactionPoolMap = JSON.parse(body);
+
+        console.log('replace transaction pool map on a sync with', rootTransactionPoolMap);
+        transactionPool.setMap(rootTransactionPoolMap);
+    }
     });
 };
 
@@ -49,7 +128,7 @@ app.listen(3000, () => {
     console.log(`localhost: ${PORT}`);
 
     if(PORT !== DEFAULT_PORT) {
-        syncChains();
+        syncWithRootState();
     }
     
 });
